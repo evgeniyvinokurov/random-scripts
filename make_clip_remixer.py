bf60b14b16568594c6abc6d5fe4c1eb5b3677f1a
Evgeniy Vinokurov <evgeniy.vinokuroff@yandex.ru>
2026-06-21 16:26:58 +0300

    changes repeat remixer

--- make_clip_remixer.py
+++ make_clip_remixer.py
@@ -2,10 +2,11 @@ from lib.remixer import ReMixer
 
 settings = {	
 	"folders": ["/home/evgenii/2023","/home/evgenii/2024","/home/evgenii/2025", "/media/evgenii/TOSHIBA EXT/zhenya/zhszh", "/media/evgenii/85799339-6cf7-41b9-902d-ac6601c1dc21/2026"],	
-	"mfolders": ["/home/evgenii/Desktop/all/before work/music folder/"],
-	"seconds": [1, 1.5, 1.7, 2,3, 4.2, 4.6],	
+	"mfolders": ["/home/evgenii/Desktop/all/music folder"],
+	"seconds": [1, 1.5, 1.7, 2,3, 4.2, 4.6, 5, 2.5, 3.4],
 	"flags": ["horizontal", "song", "8ball"]
 }
+
 
 print(settings)
 rem = ReMixer(settings)
--- play_remixer.py
+++ play_remixer.py
@@ -2,17 +2,18 @@ from lib.remixer import ReMixer
 
 settings = {	
 	"folders": ["/home/evgenii/2023","/home/evgenii/2024","/home/evgenii/2025", "/media/evgenii/TOSHIBA EXT/zhenya/zhszh", "/media/evgenii/85799339-6cf7-41b9-902d-ac6601c1dc21/2026"],	
-	"mfolders": ["/media/evgenii/TOSHIBA EXT/zhenya/старое/oldsystems/debian/хлам/остальное/music/"],
-	"seconds": [1, 1.5, 1.7, 2,3, 4.2, 4.6],
+	"mfolders": ["/home/evgenii/Desktop/all/music folder"],
+	"seconds": [1, 1.5, 1.7, 2,3, 4.2, 4.6, 5, 2.5, 3.4],
 	"flags": ["horizontal", "song", "8ball"]
 }
 
 print(settings)
-rem = ReMixer(settings)
 
 count = 10
 i = 0
+
 while i	< count:
+	rem = ReMixer(settings)
 	x = rem.run()
 	i = i + 1
 	
